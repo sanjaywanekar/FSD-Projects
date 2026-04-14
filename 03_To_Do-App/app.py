28733a6087c2270376a4d25d23f50667b451b62a
Sanjay Wanekar <sanjaywanekar34@gmail.com>
2026-04-14 23:23:19 +0530

    Fixed __main__ bug

--- 03_To_Do-App/app.py
+++ 03_To_Do-App/app.py
@@ -82,4 +82,6 @@ def delete_task(id):
     return jsonify({"message": "Deleted"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
